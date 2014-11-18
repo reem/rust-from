6edf864acb4f8c3ab54a48b7e96fc1751470d849
Jonathan Reem <jonathan.reem@gmail.com>
2014-11-17 20:04:14 -0800

    (feat) From, a trait for general conversion.

---
+++ src/lib.rs
@@ -0,0 +1,36 @@
+#![license = "MIT"]
+#![deny(missing_docs)]
+#![deny(warnings)]
+
+//! A simple general coercion trait.
+
+/// General conversion between two types.
+pub trait From<I> {
+    /// Convert I to Self.
+    fn from(I) -> Self;
+}
+
+impl<T> From<T> for T {
+    fn from(x: T) -> T { x }
+}
+
+impl<'a, T: Clone> From<&'a T> for T {
+    fn from(x: &'a T) -> T { x.clone() }
+}
+
+impl<'a, T> From<&'a Vec<T>> for &'a [T] {
+    fn from(x: &'a Vec<T>) -> &'a [T] { x.as_slice() }
+}
+
+impl<'a> From<&'a String> for &'a str {
+    fn from(x: &'a String) -> &'a str { x.as_slice() }
+}
+
+impl<'a> From<&'a str> for String {
+    fn from(x: &'a str) -> String { x.into_string() }
+}
+
+impl<'a, T: Clone> From<&'a [T]> for Vec<T> {
+    fn from(x: &'a [T]) -> Vec<T> { x.to_vec() }
+}
+
